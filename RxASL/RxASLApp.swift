b519d32d9e42514d934d3d2e9583cbcd0b324e30
Adira Blumenthal <aebdhb@rit.edu>
2023-04-19 12:48:44 -0400

    Initial Commit

---
+++ RxASL/RxASLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxASLApp.swift
+//  RxASL
+//
+//  Created by Adira Blumenthal on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RxASLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
